b681d03052a3862f3d21bfc862f2a409a8f6a2f4
ThatFinn <finn@goebl.cc>
2024-12-27 20:18:59 +0100

    Updt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,6 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ThatFinnDeV', // Usually your GitHub org/user name.
-  projectName: 'sr2e-web.github.io', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: "main",
   onBrokenLinks: 'throw',
